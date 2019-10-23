ac89a739c31c3baab702fa4fb7d06a5d210464cb
Elena Yakunicheva <jhelena@yandex.ru>
2019-10-23 13:25:57 +0300

    Flask-Migration и регистрация пользователей

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
-#from flask_login import current_user, login_required
+from flask import Blueprint, flash, render_template, redirect, url_for
+from flask_login import current_user, login_required
+from webapp.db import db
 from webapp.user.decorators import admin_required
 from webapp.user.models import Users
+from webapp.user.forms import LoginForm, RegisrationForm
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -14,3 +16,23 @@ def admin_index():
     return render_template('admin/admin.html', page_title=title, users_list=users_list)
     #return render_template('login.html', page_title=title)
 
+@blueprint.route('/register')
+def register():
+    #if current_user.is_authenticated:
+        #return redirect(url_for('index'))
+    form = RegisrationForm()
+    title = "Регистрация"
+    return render_template('admin/registration.html', page_title=title, form=form)
+
+@blueprint.route('/process-reg', methods=['POST'])
+def process_reg():
+    form = RegisrationForm()
+    if form.validate_on_submit():
+        new_user = Users(user_name=form.user_name.data, role=form.role.data, full_name=form.full_name.data, id_major=form.major.data, id_prof=form.prof.data, id_depart=form.depart.data,)
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!')
+        return redirect(url_for('user.login'))
+    flash('Пожалуйста, исправьте ошибки в форме')
+    return redirect(url_for('admin.register'))
--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
+#from wtforms.validators import DataRequired, Email, EqualTo  # было бы с Email, если бы у нас был email в форме регистрации 
 
 class LoginForm(FlaskForm):
     username = StringField("Имя пользователя", validators=[DataRequired()], render_kw={"class": "form-control"})
     password = PasswordField("Пароль", validators=[DataRequired()], render_kw={"class": "form-control"})
     remember_me = BooleanField('Запомнить меня', default=True, render_kw={"class": "form-check-input"})
+    submit = SubmitField("Отправить", render_kw={"class": "btn btn-primary"})
+
+
+class RegisrationForm(FlaskForm):
+    user_name = StringField("Имя пользователя", validators=[DataRequired()], render_kw={"class": "form-control"})
+    password = PasswordField("Пароль", validators=[DataRequired()], render_kw={"class": "form-control"})
+    password2 = PasswordField("Пароль", validators=[DataRequired(), EqualTo('password')], render_kw={"class": "form-control"})
+    role = StringField("Роль пользователя", validators=[DataRequired()], render_kw={"class": "form-control"})
+    full_name = StringField("Полное имя", validators=[DataRequired()], render_kw={"class": "form-control"})
+    major = StringField("Направление", render_kw={"class": "form-control"}) #убрали валидатор, т.к. не всегда заполняем и далее тоже
+    prof = StringField("Профиль", render_kw={"class": "form-control"})
+    depart = StringField("Кафедра", render_kw={"class": "form-control"})
+
     submit = SubmitField("Отправить", render_kw={"class": "btn btn-primary"})
